bf0436039dadc54a4bf85893cf55d89c8fc7e5fc
Mahefaa <hei.mahefa@gmail.com>
2023-07-31 10:42:06 +0300

    chore: update domain employee

--- src/main/java/com/studies/prog4/model/Employee.java
+++ src/main/java/com/studies/prog4/model/Employee.java
@@ -1,7 +1,10 @@
 package com.studies.prog4.model;
 
+import com.studies.prog4.repository.model.EmployeePhone;
+import com.studies.prog4.repository.model.NIC;
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,4 +26,27 @@ public class Employee implements Serializable {
   private String lastName;
   private LocalDate birthDate;
   private String profilePicture;
+  private com.studies.prog4.repository.model.Employee.Sex sex;
+
+  private com.studies.prog4.repository.model.Employee.Csp csp;
+
+  private String address;
+
+  private String emailPro;
+
+  private String emailPerso;
+
+  private String role;
+
+  private Integer childNumber;
+
+  private LocalDate hiringDate;
+
+  private LocalDate departureDate;
+
+  private String cnaps;
+
+  private List<EmployeePhone> phones;
+
+  private NIC nic;
 }
