22fcc9385da5ac5b1681c81bf73d8cad416e87bb
Mahefaa <hei.mahefa@gmail.com>
2023-07-31 16:11:17 +0300

    feat: implement Authenticated Request Security

---
+++ src/main/java/com/studies/prog4/controller/AuthenticatedResourceController.java
@@ -0,0 +1,33 @@
+package com.studies.prog4.controller;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class AuthenticatedResourceController implements HandlerInterceptor {
+  @ModelAttribute
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+      throws Exception {
+    Cookie[] cookies = request.getCookies();
+    boolean validToken = false;
+    if (cookies != null) {
+      for (Cookie cookie : cookies) {
+        if (cookie.getName().equals("token")) {
+          String token = cookie.getValue();
+          if (!token.isEmpty()) {
+            validToken = true;
+            break;
+          }
+        }
+      }
+    }
+    if (!validToken) {
+      response.sendRedirect("/");
+      return false;
+    }
+    return true;
+  }
+}
