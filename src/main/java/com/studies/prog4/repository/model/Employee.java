538b0f297ca15f415f13103a8fdccac13e2a9e5f
Mahefaa <hei.mahefa@gmail.com>
2023-07-31 10:08:35 +0300

    chore: add Employee Entity

--- src/main/java/com/studies/prog4/repository/model/Employee.java
+++ src/main/java/com/studies/prog4/repository/model/Employee.java
@@ -1,11 +1,16 @@
 package com.studies.prog4.repository.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -29,4 +34,45 @@ public class Employee implements Serializable {
   private String lastName;
   private LocalDate birthdate;
   private String profilePicture;
+
+  @Enumerated(EnumType.STRING)
+  private Sex sex;
+
+  @Enumerated(EnumType.STRING)
+  private Csp csp;
+
+  @Column(nullable = false)
+  private String address;
+
+  @Column(nullable = false, unique = true)
+  private String emailPro;
+
+  @Column(nullable = false, unique = true)
+  private String emailPerso;
+
+  @Column(nullable = false)
+  private String role;
+
+  @Column(nullable = false)
+  private Integer childNumber;
+
+  @Column(nullable = false)
+  private LocalDate hiringDate;
+
+  private LocalDate departureDate;
+
+  @Column(nullable = false)
+  private String cnaps;
+
+  @OneToMany(mappedBy = "employee")
+  private List<EmployeePhone> phones;
+
+  public enum Sex {
+    M, F
+  }
+
+  public enum Csp {
+    M1, M2, OS1, OS2, OS3, OP1
+
+  }
 }
