85fbddb007b4975e0ecce418ac8e58c9dafd2d8e
Mahefaa <hei.mahefa@gmail.com>
2023-07-31 09:28:55 +0300

    chore: create nic table

---
+++ src/main/resources/db/migration/V1_3__create_nic_card_table_and_link_with_employee.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS nic
+(
+    uuid          uuid
+        constraint nic_pk primary key default uuid_generate_v4(),
+    id            varchar unique not null,
+    issuing_date  date           not null,
+    issuing_place varchar        not null
+);
+
+ALTER TABLE employee
+    ADD COLUMN nic_id uuid;
+ALTER TABLE employee
+    ADD CONSTRAINT fk_nic foreign key (nic_id) references nic (id);
