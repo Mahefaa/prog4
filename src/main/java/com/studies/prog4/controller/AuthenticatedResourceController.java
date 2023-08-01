7bd39b5851b436973ca4fc1339af5fb2942561f5
Mahefaa <hei.mahefa@gmail.com>
2023-08-01 10:11:02 +0300

    chore: logout

--- src/main/java/com/studies/prog4/controller/AuthenticatedResourceController.java
+++ src/main/java/com/studies/prog4/controller/AuthenticatedResourceController.java
@@ -4,8 +4,11 @@ import com.studies.prog4.service.CompanyService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+@Controller
 public class AuthenticatedResourceController extends GlobalResourceProvider {
   public AuthenticatedResourceController(CompanyService companyService) {
     super(companyService);
@@ -30,5 +33,19 @@ public class AuthenticatedResourceController extends GlobalResourceProvider {
     if (!validToken) {
       response.sendRedirect("/login");
     }
+  }
+
+  @GetMapping("/logout")
+  private String logout(HttpServletRequest request, HttpServletResponse response) {
+    Cookie[] cookies = request.getCookies();
+    if (cookies != null) {
+      for (Cookie cookie : cookies) {
+        if (cookie.getName().equals("token")) {
+          cookie.setMaxAge(0);
+          response.addCookie(cookie);
+        }
+      }
+    }
+    return "redirect:/login";
   }
 }
